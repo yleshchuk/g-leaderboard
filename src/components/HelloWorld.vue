26575bd7b10528be34d2a5cc1e899127d8a7aa2a
Yaroslav Leshchuk <yles@keplercode.com>
2023-09-27 12:49:42 +0300

    initial

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-container class="fill-height">
+    <v-responsive class="align-center text-center fill-height">
+      <v-table theme="dark">
+        <thead>
+          <tr>
+            <th class="text-left">Name</th>
+            <th class="text-left">Best</th>
+            <th class="text-left">Total</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="item in players" :key="item.name">
+            <td>{{ item.name }}</td>
+            <td>{{ item.total }}</td>
+            <td>{{ item.best }}</td>
+          </tr>
+        </tbody>
+      </v-table>
+    </v-responsive>
+  </v-container>
+</template>
+
+<script lang="ts" setup>
+const players = [{ name: "Yarko", best: 15555, total: 35000 }];
+</script>
---
+++ src/layouts/default/AppBar.vue
@@ -0,0 +1,7 @@
+<template>
+  <v-app-bar flat> </v-app-bar>
+</template>
+
+<script lang="ts" setup>
+//
+</script>
